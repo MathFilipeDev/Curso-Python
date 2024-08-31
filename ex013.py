cf01440f44c03aed40adf068ec29f763e380c027
Matheus Filipe <166725411+MathFilipeDev@users.noreply.github.com>
2024-08-30 22:47:09 -0300

    [feat] Adicionando arquivos

---
+++ ex013.py
@@ -0,0 +1,4 @@
+S = float(input('Qual é o salário do Funcionário? R$ '))
+A = (S*0.15)
+NS = (S+A)
+print(f'Um funcionário que ganhava R${S:.2f}, com 15% de aumento, passa a receber R${NS:.2f}') 
---
+++ ex014.py
@@ -0,0 +1,3 @@
+C = float(input('Informe a temperatura em °C: '))
+V = ((C * 9) / 5) + 32
+print(f'A temperatura de {C:.1f}°C corresponte a {V:.1f}°F')
