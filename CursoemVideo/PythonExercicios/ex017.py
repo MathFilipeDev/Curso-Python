e82979d6c1650ca47af6e6770f2211bbdfc4b9a4
Matheus Filipe <166725411+MathFilipeDev@users.noreply.github.com>
2024-09-01 14:17:02 -0300

    Add files via upload

---
+++ CursoemVideo/PythonExercicios/ex017.py
@@ -0,0 +1,6 @@
+#Primeira maneira (básica)
+#b = float(input('Comprimento do cateto oposto: '))
+#c = float(input('Comprimento do cateto adjacente: '))
+#print(f'A hipotenusa vai medir {((b**2)+(c**2))**0.5:.2f}')
+
+import math
