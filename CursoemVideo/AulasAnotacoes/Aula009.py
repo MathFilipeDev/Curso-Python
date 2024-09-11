2bd4d5b3d941b0d8d1104bf3a38971c1f1d9751c
Matheus Filipe <math.filipe23@gmail.com>
2024-09-10 21:32:25 -0300

    [feat] change-files

--- CursoemVideo/AulasAnotacoes/Aula009.py
+++ CursoemVideo/AulasAnotacoes/Aula009.py
@@ -1,4 +1,21 @@
 # Fatiamento de String
-
 frase = 'Curso em Vídeo Python'
-print(frase[3:13])
+print(frase[9])       # 'V'
+print(frase[9:13])    # 'Víde'
+print(frase[9:21:2])  # 'VdoPto'
+
+#Funções de Análise
+frase = 'Curso em Vídeo Python'
+print(len(frase))         # 21
+print(frase.count('o'))   # 3
+print(frase.find('Vídeo')) # 9
+
+#Transformações
+frase = '   Aprendendo Python   '
+print(frase.replace('Python', 'Java'))  # '   Aprendendo Java   '
+print(frase.upper())                    # '   APRENDENDO PYTHON   '
+print(frase.lower())                    # '   aprendendo python   '
+print(frase.capitalize())               # '   aprendendo python   '
+print(frase.title())                    # '   Aprendendo Python   '
+print(frase.strip())                    # 'Aprendendo Python'
+
