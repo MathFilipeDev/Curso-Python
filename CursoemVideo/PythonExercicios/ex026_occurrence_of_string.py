bf6761e9cd26668fcd370e50212db6038b9c4d67
Matheus Filipe <math.filipe23@gmail.com>
2024-09-18 10:46:40 -0300

    Update ex026_occurrence_of_string.py

--- CursoemVideo/PythonExercicios/ex026_occurrence_of_string.py
+++ CursoemVideo/PythonExercicios/ex026_occurrence_of_string.py
@@ -1,5 +1,4 @@
 F = str(input('Digite uma frase: '))
-L = (F.count)
-print(f'A letra A aparece {L} vezes.')
-print(f'A primeira letra A apareceu na posição {L}')
-print(f'A última letra A apareceu na posição {L}')
+print(f'A letra A aparece {F.count('a')} vezes.')
+print(f'A primeira letra A apareceu na posição {F}')
+print(f'A última letra A apareceu na posição {F}')
