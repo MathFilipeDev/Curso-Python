bea242b0aa45b1d0f4d9920c09d2fab7bf6320dd
Matheus Filipe <math.filipe23@gmail.com>
2024-09-19 15:15:37 -0300

    [fix] change-code

--- CursoemVideo/PythonExercicios/ex026_occurrence_of_string.py
+++ CursoemVideo/PythonExercicios/ex026_occurrence_of_string.py
@@ -1,4 +1,5 @@
-F = str(input('Digite uma frase: '))
-print(f'A letra A aparece {F.count('a')} vezes.')
-print(f'A primeira letra A apareceu na posição {F.find('a')}')
-print(f'A última letra A apareceu na posição {F.rfind('a')}')
+F = str(input('Digite uma frase: ')).upper().strip()
+#.strip não conta os espaços
+print(f'A letra A aparece {F.count('A')} vezes.')
+print(f'A primeira letra A apareceu na posição {F.find('A')+1}')
+print(f'A última letra A apareceu na posição {F.rfind('A')+1}')
