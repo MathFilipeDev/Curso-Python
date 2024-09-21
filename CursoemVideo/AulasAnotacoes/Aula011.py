42bcab1f6c6dcd35cc54d85adc58aa1ae2c2e1b6
Matheus Filipe <math.filipe23@gmail.com>
2024-09-20 23:52:46 -0300

    [feat] create-aula011

---
+++ CursoemVideo/AulasAnotacoes/Aula011.py
@@ -0,0 +1,36 @@
+#TABELA DE CORES NO TERMINAL:
+#\033[style;text;backm
+
+style = ('Define o estilo do texto (negrito, sublinhado, etc.)')
+text = ('Define a cor do texto.')
+back = ('Define a cor de fundo.')
+
+#Estilos de Texto:
+0 = ('Nenhum (padrão)')
+1 = ('Negrito')
+4 = ('Sublinhado')
+7 = ('Inverte as cores (fundo vira o texto e vice-versa)')
+
+#Cores de Texto:
+
+30 = ('Preto')
+31 = ('Vermelho')
+32 = ('Verde')
+33 = ('Amarelo')
+34 = ('Azul')
+35 = ('Magenta')
+36 = ('Ciano')
+37 = ('Branco')
+
+#Cores de Fundo:
+
+40 = ('Preto')
+41 = ('Vermelho')
+42 = ('Verde')
+43 = ('Amarelo')
+44 = ('Azul')
+45 = ('Magenta')
+46 = ('Ciano')
+47 = ('Branco')
+
+#----------------------------------------------------------------------------------#
---
+++ CursoemVideo/AulasAnotacoes/Aula011_practice.py
@@ -0,0 +1,10 @@
+#EXEMPLOS CORES NO TERMINAL:
+print('\033[31mOlá, Mundo!')
+print('\033[31;43mOlá, Mundo!')
+print('\033[1;31;43mOlá, Mundo!')
+print('\033[4;37;45mOlá, Mundo!')
+
+#Para funcionar corretamente coloque o '#' no código de cima.
+a = 3
+b = 5
+print(f'Os valores são \033[32m{a}\033[m e \033[31m{b}\033[m')
